85ad2d39a2c92542a841b84b50f747d039ee54f4
chananup <chananup@sheridancollege.ca>
2021-02-05 14:11:24 -0500

    Added new object to CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,15 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * Name:Anupama Chandran
+ * Student Id:991589328
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card c1 = new Card();
+       c1.setSuit("Spades");
+       c1.setValue(1);
     }
     
 }
