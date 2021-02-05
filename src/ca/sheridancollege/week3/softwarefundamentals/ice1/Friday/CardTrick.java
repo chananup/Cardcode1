a0e2c66af7e6a81651949879f625e5df97c82e84
chananup <chananup@sheridancollege.ca>
2021-02-05 14:57:41 -0500

    Added comments to CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -9,14 +9,21 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
+       //Creating Object c1
        Card c1 = new Card();
+       
+       //Setting values to Suit and Value in Object c1
        c1.setSuit("Spades");
        c1.setValue(1);
-        
+       
+       //Creating Object c2
        Card c2 = new Card();
+       
+       //Setting values to Suit and Value in Object c2
        c2.setSuit("Diamonds");
        c2.setValue(2);
        
+       //Printing Object c1 Suit and Value
         System.out.println("Suit is "+ c1.getSuit());
          System.out.println("Suit is "+ c1.getValue());
     }
